7ba636472f50e72a6ecd7f777da187cde035188f
min <sensemin7@gmail.com>
2021-10-20 15:32:06 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 
 const customers = [
 {
@@ -29,13 +48,60 @@ const customers = [
 }
 ]
 
-function App() {
+// function App() {
+//   const { classes } = this.props;
+//   return (
+//     <Paper className={classes.root}>
+//       <Table className={classes.table}>
+//         <TableHead>
+//           <TableCell>번호</TableCell>
+//           <TableCell>이미지</TableCell>
+//           <TableCell>이름</TableCell>
+//           <TableCell>생년월일</TableCell>
+//           <TableCell>성별</TableCell>
+//           <TableCell>직업</TableCell>
+//         </TableHead>
+//         <TableBody>
+//         { 
+//           customers.map(c => {
+//             return (
+//               <Customer 
+//                 key={c.id}
+//                 id={c.id}
+//                 image={c.image}
+//                 name={c.name}
+//                 birthday={c.birthday}
+//                 gender={c.gender}
+//                 job={c.job}
+//               />
+//             )
+//           })
+//         }
+//         </TableBody>
+//       </Table>
+//     </Paper>
+//   );
+// }
+
+class App extends Component {
+  render() {
+  const { classes } = this.props;
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer 
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+        </TableHead>
+        <TableBody>
+        { 
+          customers.map(c => {
+            return (
+              <Customer 
                 key={c.id}
                 id={c.id}
                 image={c.image}
@@ -43,20 +109,15 @@ function App() {
                 birthday={c.birthday}
                 gender={c.gender}
                 job={c.job}
-            />
-          )
-        })
-      }
-    </div>
+              />
+            )
+          })
+        }
+        </TableBody>
+      </Table>
+    </Paper>
   );
+  }
 }
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <Customer />
-//     );
-//   }
-// }
-
-export default App;
+export default withStyles(styles)(App);
